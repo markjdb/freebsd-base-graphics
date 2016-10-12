6cc6ca6c705c2713f090ed04c51fe6841fd7e7cb
Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
2016-10-12 10:18:54 +0300

    drm/i915: Remove unused BSM_MASK causing warning
    Remove never used BSM{,_MASK}. BSM_MASK #define also causes a warning.
    
    include/drm/i915_drm.h:96:34: warning: result of ‘65535 << 20’
    requires 37 bits to represent, but ‘int’ only has 32 bits
    [-Wshiftoverflow=]
       #define   INTEL_BSM_MASK (0xFFFF << 20)
    
    Reported-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: http://patchwork.freedesktop.org/patch/msgid/1476256734-6457-1-git-send-email-joonas.lahtinen@linux.intel.com
    Signed-off-by: Johannes Lundberg <johalun0@gmail.com>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -92,9 +92,6 @@ static inline bool i915_mmio_reg_valid(i915_reg_t reg)
 #define DEVEN 0x54
 #define   DEVEN_MCHBAR_EN (1 << 28)
 
-#define BSM 0x5c
-#define   BSM_MASK (0xFFFFUL << 20)
-
 #define HPLLCC	0xc0 /* 85x only */
 #define   GC_CLOCK_CONTROL_MASK		(0x7 << 0)
 #define   GC_CLOCK_133_200		(0 << 0)
