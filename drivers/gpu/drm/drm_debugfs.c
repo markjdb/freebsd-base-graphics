9af2ace88102fd8a620343eca109a2852c6080ed
Liviu Dudau <Liviu.Dudau@arm.com>
2016-11-17 11:41:29 +0000

    drm/atomic: cleanup debugfs entries on un-registering the driver.
    Cleanup the debugfs entries created by
    commit 6559c901cb48:  drm/atomic: add debugfs file to dump out atomic state
    when the driver's minor gets un-registered. Without it, DRM drivers
    compiled as modules cannot be rmmod-ed and modprobed again.
    
    Tested-by: Brian Starkey <brian.starkey@arm.com>
    Signed-off-by: Liviu Dudau <Liviu.Dudau@arm.com>
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Link: http://patchwork.freedesktop.org/patch/msgid/20161117114129.2627-1-Liviu.Dudau@arm.com
    Fixes: 6559c901cb48 ("drm/atomic: add debugfs file to dump out atomic state")
    Signed-off-by: Johannes Lundberg <johalun0@gmail.com>

--- drivers/gpu/drm/drm_debugfs.c
+++ drivers/gpu/drm/drm_debugfs.c
@@ -236,6 +236,7 @@ EXPORT_SYMBOL(drm_debugfs_remove_files);
 int drm_debugfs_cleanup(struct drm_minor *minor)
 {
 	struct drm_device *dev = minor->dev;
+	int ret;
 
 	if (!minor->debugfs_root)
 		return 0;
@@ -244,7 +245,16 @@ int drm_debugfs_cleanup(struct drm_minor *minor)
 		dev->driver->debugfs_cleanup(minor);
 
 	/* NOTE Linux<->FreeBSD: Mesa needs the hw.dri sysctl tree. */
+#ifdef __FreeBSD__ // XXX: Really?
 	drm_sysctl_cleanup(minor->dev);
+#endif
+	if (drm_core_check_feature(dev, DRIVER_ATOMIC)) {
+		ret = drm_atomic_debugfs_cleanup(minor);
+		if (ret) {
+			DRM_ERROR("DRM: Failed to remove atomic debugfs entries\n");
+			return ret;
+		}
+	}
 
 	drm_debugfs_remove_files(drm_debugfs_list, DRM_DEBUGFS_ENTRIES, minor);
 
