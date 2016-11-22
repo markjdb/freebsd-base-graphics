aa2008e7c151f79065252eee0f4ac7988f14acdd
Chris Wilson <chris@chris-wilson.co.uk>
2016-11-22 14:41:18 +0000

    drm/i915: Don't deref context->file_priv ERR_PTR upon reset
    When a user context is closed, it's file_priv backpointer is replaced by
    ERR_PTR(-EBADF); be careful not to chase this invalid pointer after a
    hang and a GPU reset.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Fixes: b083a0870c79 ("drm/i915: Add per client max context ban limit")
    Cc: Mika Kuoppala <mika.kuoppala@intel.com>
    Reviewed-by: Mika Kuoppala <mika.kuoppala@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20161122144121.7379-1-chris@chris-wilson.co.uk
    Signed-off-by: Johannes Lundberg <johalun0@gmail.com>

--- drivers/gpu/drm/i915/i915_gem.c
+++ drivers/gpu/drm/i915/i915_gem.c
@@ -2754,16 +2754,12 @@ static void i915_gem_context_mark_guilty(struct i915_gem_context *ctx)
 	DRM_DEBUG_DRIVER("context %s marked guilty (score %d) banned? %s\n",
 			 ctx->name, hs->ban_score, yesno(hs->banned));
 
-	if (!ctx->file_priv)
+	if (!ctx->banned || IS_ERR_OR_NULL(ctx->file_priv))
 		return;
 
-	if (hs->banned) {
-		ctx->file_priv->context_bans++;
-
-		DRM_DEBUG_DRIVER("client %s has has %d context banned\n",
-				 ctx->name,
-				 ctx->file_priv->context_bans);
-	}
+	ctx->file_priv->context_bans++;
+	DRM_DEBUG_DRIVER("client %s has had %d context banned\n",
+			 ctx->name, ctx->file_priv->context_bans);
 }
 
 static void i915_gem_context_mark_innocent(struct i915_gem_context *ctx)
