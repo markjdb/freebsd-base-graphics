543111372826062469002909b7e517881ca4a407
Chris Wilson <chris@chris-wilson.co.uk>
2017-01-06 15:20:11 +0000

    drm/i915: Use fixed-sized types for stolen
    Stolen memory is a hardware resource of known size, so use an accurate
    fixed integer type rather than the ambiguous variable size_t. This was
    motivated by the next patch spotting inconsistencies in our types.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20170106152013.24684-3-chris@chris-wilson.co.uk
    Signed-off-by: Johannes Lundberg <johalun0@gmail.com>

--- drivers/gpu/drm/i915/i915_gem_gtt.c
+++ drivers/gpu/drm/i915/i915_gem_gtt.c
@@ -3152,8 +3152,10 @@ static int i915_gmch_probe(struct i915_ggtt *ggtt)
 	ggtt->mappable_base = gtt->gma_bus_addr;
 	ggtt->mappable_end = gtt->gtt_mappable_entries << PAGE_SHIFT;
 #else
-	intel_gtt_get(&ggtt->base.total, &ggtt->stolen_size,
-		      &ggtt->mappable_base, &ggtt->mappable_end);
+	intel_gtt_get(&ggtt->base.total,
+				  &ggtt->stolen_size,
+				  &ggtt->mappable_base,
+				  &ggtt->mappable_end);
 #endif
 
 	ggtt->do_idle_maps = needs_idle_maps(dev_priv);
@@ -3219,7 +3221,7 @@ int i915_ggtt_probe_hw(struct drm_i915_private *dev_priv)
 	DRM_INFO("Memory usable by graphics device = %lluM\n",
 		 ggtt->base.total >> 20);
 	DRM_DEBUG_DRIVER("GMADR size = %lldM\n", ggtt->mappable_end >> 20);
-	DRM_DEBUG_DRIVER("GTT stolen size = %zdM\n", ggtt->stolen_size >> 20);
+	DRM_DEBUG_DRIVER("GTT stolen size = %uM\n", ggtt->stolen_size >> 20);
 #ifdef CONFIG_INTEL_IOMMU
 	if (intel_iommu_gfx_mapped)
 		DRM_INFO("VT-d active for gfx access\n");
@@ -3576,4 +3578,3 @@ i915_get_ggtt_vma_pages(struct i915_vma *vma)
 
 	return ret;
 }
-
